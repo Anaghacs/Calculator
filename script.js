d75ae6f613ad15706534dcb0cc5757f89843a7a0
akshaycs435 <anaghasasidharan38@gmail.com>
2023-11-19 17:22:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicking(val){
+    document.getElementById("screen").value+=val
+}
+function clearScreen(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
