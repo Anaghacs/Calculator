08d2ddd8134986f6f8e7018476722ea9294fff79
akshaycs435 <anaghasasidharan38@gmail.com>
2023-11-20 15:43:03 +0530

    change the style and jscode

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function clicking(val){
-    document.getElementById("screen").value+=val
+    document.getElementById("screen").value+=val;
 }
 function clearScreen(){
-    document.getElementById("screen").value=""
+    document.getElementById("screen").value="";
 }
 function equalClick(){
-    var text=document.getElementById("screen").value
-    var result=eval(text)
-    document.getElementById("screen").value=result
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value=result;
 }
+function oneClearClick(){
+
+    display=document.getElementById("screen").value.slice(0,-1);
+    document.getElementById("screen").value=display;
+}
+
+
+
+
+
